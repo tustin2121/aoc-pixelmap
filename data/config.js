769f7de133459a3872a8a20eb4a8b5c760379922
Tustin2121 <tustin2121@gmail.com>
2019-10-12 15:25:41 -0400

    First proper data files

--- data/config.js
+++ data/config.js
@@ -13,18 +13,18 @@ var mapConfig = {
 			name: "Overworld",
 			bounds: [[0,0],[0,0]],
 			layers: ["day","night","topo"],
-			center: { x:4*512+238, z:21*512+164 },
+			center: { x:0, z:0 },
 			towns: [
-				{ name: "Spawn Town", x:4*512+238, z:21*512+164, rad:50 },
+				// { name: "Spawn Town", x:4*512+238, z:21*512+164, rad:50 },
 			],
 			routes: [
-				{ name: "Route 1", path:[[2352,10848], [2724,10749]] },
+				// { name: "Route 1", path:[[2352,10848], [2724,10749]] },
 			],
 			areas: [
-				{ name: "Fields of Wheat", boundery:[[2250,10988], [2367,10966], [2401,11041], [2369,11094], [2306,11093]] },
+				// { name: "Fields of Wheat", boundery:[[2250,10988], [2367,10966], [2401,11041], [2369,11094], [2306,11093]] },
 			],
 			pois: [
-				{ name: "Crator Lake", x:2594, z:11186 },
+				// { name: "Crator Lake", x:2594, z:11186 },
 			],
 		},
 	},
